86c1c874d39ddb86229526ad824a56192edd27fb
DrissBelaroussi <driss.belaroussi.sio@gmail.com>
2018-03-22 14:22:29 +0100

    API Methode GET

---
+++ APIWeb/Models/Produit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace APIWeb.Models
+{
+    public class Produit
+    {
+        public int id;
+        public string name;
+        public decimal price; 
+
+        public Produit()
+        {
+        }
+
+        public Produit(int id, string name, decimal price)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+        }
+
+
+        public override string ToString()
+        {
+            return string.Format("[Produit: id={0}, name={1}, price={2}]", id, name, price);
+        }
+
+    }
+}
---
+++ APIWeb/Services/ProductRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using APIWeb.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace APIWeb.Services
+{
+        public class ProductRepository
+        {
+
+            public Produit[] GetAllProducts()
+            {
+           
+                    return new Produit[]
+                {
+                    new Produit
+                    {
+                        id = 1,
+                        name = "Paquet de bonbons",
+                        price = 3.5m
+                    },
+                     new Produit
+                    {
+                        id = 2,
+                        name = "Serpillère",
+                        price = 10.25m
+                    }
+                    };
+              }
+
+        }
+
+}
